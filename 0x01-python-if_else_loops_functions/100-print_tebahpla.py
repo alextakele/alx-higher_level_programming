1dbee4d33046d28ed9027fdf52c15e1c74616dd0
alexa221 <alextakele16@gmail.com>
2022-06-28 13:36:05 +0300

    Advanced Task 1

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 100-print_tebahpla.py
-# Alex  Takele <375@alx.com>
+# Brennan D Baraban <375@holbertonschool.com>
 
 """"Print the alphabet in reverse order alternating upper- and lower-case."""
 i = 0
