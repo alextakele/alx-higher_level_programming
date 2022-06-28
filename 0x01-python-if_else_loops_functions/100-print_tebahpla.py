262b1eff880335b7199b46ce1bba50f232344997
alexa221 <alextakele16@gmail.com>
2022-06-28 13:58:54 +0300

    Advanced one

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for c in range(ord('z'), ord('a') - 1, -1):
-    if (c % 2 != 0):
-        c = c - 32
-    print("{:c}".format(c), end='')
+for letter in range(25, -1, -1):
+    alphabet = letter + ord('A')
+    if letter % 2 == 1:
+        alphabet += 32
+    print('{:c}'.format(alphabet), end='')
