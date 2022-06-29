622fc59d562238d65a6cbf4217eb8753dbc0bb1d
alexa221 <alextakele16@gmail.com>
2022-06-29 16:56:02 +0300

    Advanced task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,1 +1,7 @@
-
+#!/usr/bin/python
+for i in reversed(range(97, 123):
+        if i% 2 == 0:
+           print('{:c}'.format(i), end='')
+           else:
+           print('{:c}'.formt(i-32), end='')
+        
