cae57a5fc36f2d89eced40a17a7dae00e8f310fd
alexa221 <alextakele16@gmail.com>
2022-06-29 09:03:11 +0300

    Four tasks

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for letter in range(25, -1, -1):
-    alphabet = letter + ord('A')
-    if letter % 2 == 1:
-        alphabet += 32
-    print('{:c}'.format(alphabet), end='')
+for i in reversed(range(97, 123)):
+    if i % 2 == 0:
+        print('{:c}'.format(i), end='')
+    else:
+        print('{:c}'.format(i-32), end='')
