8faf34d7d878f5b3c070ae692e73bfc751707341
alexa221 <alextakele16@gmail.com>
2022-07-01 15:37:16 +0300

    Task 4

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
@@ -7,19 +5,19 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    a = int(argv[1])
-    op = argv[2]
-    b = int(argv[3])
-    if op == "+":
-        result = add(a, b)
-    elif op == "-":
-        result = sub(a, b)
-    elif op == "*":
-        result = mul(a, b)
-    elif op == "/":
-        result = div(a, b)
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-    print("{:d} {} {:d} = {}".format(a, op, b, result))
->>>>>>> cf47e4b2a6d3474a5ee13ec65200b8a6673fff0e
+        a = int(argv[1])
+        op = argv[2]
+        b = int(argv[3])
+        if op == "+":
+            result = add(a, b)
+        elif op == "-":
+             result = sub(a, b)
+        elif op == "*":
+             result = mul(a, b)
+        elif op == "/":
+             result = div(a, b)
+        else:
+             print("Unknown operator. Available operators: +, -, * and /")
+             exit(1)
+             print("{:d} {} {:d} = {}".format(a, op, b, result))
+
