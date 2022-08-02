e7160faaa8d9cfdff4a6830f1a16f4784902d030
alexa221 <alextakele16@gmail.com>
2022-08-02 08:37:33 +0300

    inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-"""Module.
+"""
+Module.
+
 """
 
 
 class MyList(list):
+
     """
+
     class
+    
     """
+
     def print_sorted(self):
+
         """
+
         method prints sorted list.
+
         """
         print(sorted(self))
