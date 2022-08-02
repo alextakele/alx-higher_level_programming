fd87f3225c2fed34030a91b24b8866893a0a7a80
alexa221 <alextakele16@gmail.com>
2022-08-02 13:45:14 +0300

    reads a text file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""This module defines read_file() function"""
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+    """This function reads a text file in UTF8 encoding and prints it
 
+    Param:
+        filename: name of file to read from
+    """
+    with open(filename, encoding="UTF8") as f:
+        for line in f:
+            print(line, end="")
