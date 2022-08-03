2c7bb61127ba9b23ff5cf3f4521305697981ba0e
alexa221 <alextakele16@gmail.com>
2022-08-03 08:39:59 +0300

    argument

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,20 +1,36 @@
-#!/usr/bin/python3
-"""Student class defines a student"""
+class Time:
+    def __int__(self, hour=0, minute=0, second=0):
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+    def __str__(self):
+        return "{}:{:02d}:{:02d}".format(self.hour, self.minute, self.second)
+    def __add__(self, other_time):
+        new_time= Time()
+        #Add the seconds and correct if sum > 60
+        if (self.second + other_time.second) >= 60:
+            self.minute += 1
+            new_time.second = (self.second + other_time.second)-60
+        else:
+            new_time.second = self.second - other_time.second
 
+            # Add the minutes and correct if sum > 60
+        if (self.minute + other_time.minute) >= 60:
+            self.hour += 1
+            new_time.minute=(self.minute + other_time.minute)-60
+        else:
+            new_time.minute = self.minute - other_time.minute
 
-class Student:
-    """defines a student"""
-    def __init__(self, first_name, last_name, age):
-        """instantiation"""
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+        #Add the hour and correct if sum > 24
+        if (self.hour + other_time.hour) > 24:
+            new_time.hour = (self.hour + other_time.hour)-24
+        else:
+            new_time.hour = self.hour + other_time.hour
+        return new_time
+def main():
+    Time1 = Time(1, 20, 30)
+    print(Time1)
+    Time2 = Time(24, 41, 30)
+    print(Time2)
+    print(Time1 + Time2)
 
-    def to_json(self, attrs=None):
-        """retrieves a dictionary representation of
-        Student instance if attrs is a list of stirngs, only
-        only attribute name contained in the list must be retrieved
-        else all attributes must be retrieved"""
-        if type(attrs) == list and all(type(elem) == str for elem in attrs):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__
---
+++ 0x0B-python-input_output/two.py
@@ -0,0 +1,5 @@
+# python practise file
+f = open('workfile', 'w', encoding="utf-8")
+with open('workfile', encoding="utf-8") as f:
+    read_data = f.read()
+    f.closed
