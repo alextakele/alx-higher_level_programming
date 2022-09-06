0de9bb51340cc987b78384f919f04825e9bf2d8a
alexa221 <alextakele16@gmail.com>
2022-09-06 06:55:23 -0700

    table dump

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,4 @@
--- displays average temp by city in descending order
-SELECT city, AVG(value) AS avg_temp
-FROM temperatures
-GROUP BY city
-ORDER BY avg_temp DESC;
+-- Computes the average of a group of records in a table in the database
+SELECT city, AVG(value) AS avg_temp FROM temperatures
+    GROUP BY city
+    ORDER BY avg_temp DESC;
