cef6c77079d250e1e84a358ebe993b103af076a7
alexa221 <alextakele16@gmail.com>
2022-10-25 11:43:08 -0700

    python-network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Takes in a URL, sends a GET request to the URL, displays the body of the response
-curl -sX GET "$1" -L 200
+# Script that takes a URL, sends a GET request to it, and displays the body
+curl -s --location GET "$1"
